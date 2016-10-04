94594d85636e83654e9006bebf238c86a85348a1
Clayton Hung <hung.clayton@gmail.com>
2016-10-04 01:45:55 -0400

    okay

--- app.js
+++ app.js
@@ -1,47 +1,52 @@
-"use strict";
+'use strict';
 
-const app = module.exports = require('koa')(),
-      router = require('koa-router')(),
-      mount = require('koa-mount'),
-      body = require('koa-body-parser'),
-      send = require('koa-send'),
-      serve = require('koa-static'),
-      logger = require('koa-logger');
-const handler = require('./routes/handler.js');
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const handler = require('./routes/handler');
+// const multer = require('multer');
+const app = express();
 
-// Logger
-app.use(logger());
 
-app.use(body());
-app.use(router.routes());
-app.use(router.allowedMethods());
+const allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+};
 
-// Routes
-router.post('/user', handler.updateUser);
-router.post('/searchRecipes', handler.searchRecipe);
+// var server = app.listen(3000, function () {
 
-// Additional path stripped from routes
-app.use(mount('/api/v1', router.middleware()));
+// 	//configurations
+//   app.use(express.static(path.join(__dirname, 'public')));
+//   app.use(allowCrossDomain);
+//    app.use(app.router);
+//   // app.use(express.bodyParser());
+//   app.use(bodyParser.json()); // for parsing application/json
+//   app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+//   app.use(multer()); // for parsing multipart/form-data
 
-// Static Files
-app.use(serve(__dirname + '/public'));
-app.use(function *index(){
-  yield send(this, __dirname + '/index.html');
+// 	console.log("Server is running!");
+// });
+
+app.configure(function() {
+    // app.set('port', process.env.PORT || 3000);
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
+
+    app.use(express.static(path.join(__dirname, 'public')));
+    app.use(allowCrossDomain);
+
+    console.log("Server is running!");
 });
 
-app.use(function *(next){
-  try{
-    yield next; //pass on the execution to downstream middlewares
-  } catch (err) { //executed only when an error occurs & no other middleware responds to the request
-    this.type = 'application/vnd.api+json'; //optional here
-    this.status = err.status || 500;
-    this.body = { 'error' : 'The application recieved an error;) '};
-    //delegate the error back to application
-    this.app.emit('error', err, this);
-  }
+app.get('/', function (req, res) {
+  res.redirect('/index.html');
 });
 
-if (!module.parent) {
-  app.listen(process.env.PORT || 3000);
-  console.log('Server running on http://localhost:3000/!');
-}
+app.post('/api/v1/user', handler.updateUser);
+
+app.post('/api/v1/searchRecipes', handler.searchRecipe);
+
+app.listen(process.env.PORT || 3000);
